a78ce7e6aca91be78d50558acfc63b975bc23588
etothepi16 <paulmcdowell18@gmail.com>
2017-03-15 22:40:16 -0400

    allowed for user-specified parameters

--- Source.cpp
+++ Source.cpp
@@ -15,22 +15,28 @@ int gcd(int a, int b)
 
 int main() {
 	int rand1, rand2;
-	int range = 1000000;
+	int max = 0;
 	int count = 0;
 	long double percent = 0;
-	for (int i = 0; i < 10000; i++)
+	int iterations;
+
+	cout << "How big would you like your numbers to be? ";
+	cin >> max;
+	cout << "How many iterations would you like to do? ";
+	cin >> iterations;
+	for (int i = 0; i < iterations; i++)
 	{
-		rand1 = floor((double)rand() / RAND_MAX*range);
-		rand2 = floor((double)rand() / RAND_MAX*range);
+		rand1 = floor((double)rand() / RAND_MAX*max);
+		rand2 = floor((double)rand() / RAND_MAX*max);
 		cout << "Randomly chose " << rand1 << " and " << rand2 << endl;
 		if (gcd(rand1, rand2) == 1) {
 			count++;
 		}
-		percent = ((long double)count / 10000);
+		percent = ((long double)count / iterations);
 		cout << "Current percentage of coprimes is: " << (percent*100) << endl;
 	}
 
 	long double result = sqrtl(6.000000 / percent);
-	cout << "Pi has been experimentally calculated to be: " << result << endl;
+	cout << "Pi has been experimentally calculated to be: " << result << "!\nHappy Pi Day!" << endl;
 	return 0;
 }
